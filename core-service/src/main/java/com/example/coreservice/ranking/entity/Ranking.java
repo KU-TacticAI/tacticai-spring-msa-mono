bcf72eece275e16e4d17e4ccdcb829a93001de6a
K-da-fire <105974458+K-da-fire@users.noreply.github.com>
2025-08-16 00:43:25 +0900

    랭킹 연결

--- core-service/src/main/java/com/example/coreservice/ranking/entity/Ranking.java
+++ core-service/src/main/java/com/example/coreservice/ranking/entity/Ranking.java
@@ -1,14 +1,10 @@
 package com.example.coreservice.ranking.entity;
 
 import com.example.commonmodule.base_entity.BaseEntity;
-import com.example.coreservice.config.LongListConverter;
-import jakarta.persistence.Column;
-import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -27,15 +23,15 @@ public class Ranking extends BaseEntity {
 
   private Long userId;
 
-  private String username;
+//  private String username;
 
   private Long totalScore;
 
   private Long rankOrder;
 
-  @Convert(converter = LongListConverter.class)
-  @Column(columnDefinition = "TEXT")
-  private List<Long> aiList;
+//  @Convert(converter = LongListConverter.class)
+//  @Column(columnDefinition = "TEXT")
+//  private List<Long> aiList;
 
   public void setRank(Long rank) {
     this.rankOrder = rank;
