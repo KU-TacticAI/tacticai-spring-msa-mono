4e8fef918d60028934224b27edfa123a03f86c15
K-da-fire <105974458+K-da-fire@users.noreply.github.com>
2025-08-26 11:09:23 +0900

    게임시작 구현

--- game-service/src/main/kotlin/com/example/gameservice/gamelobby/controller/LobbySocketController.kt
+++ game-service/src/main/kotlin/com/example/gameservice/gamelobby/controller/LobbySocketController.kt
@@ -1,7 +1,9 @@
 package com.example.gameservice.gamelobby.controller
 
 import com.example.gameservice.gamelobby.dto.ReadyRequestDto
+import com.example.gameservice.gamelobby.dto.RoomResponseDto
 import com.example.gameservice.gamelobby.service.LobbyService
+import org.springframework.http.ResponseEntity
 import org.springframework.messaging.handler.annotation.DestinationVariable
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.Payload
@@ -20,5 +22,13 @@ class LobbySocketController(
         @Payload readyEvent: ReadyRequestDto,
     ) {
         lobbyService.updateReady(readyEvent.getType(), readyEvent.getRoomId().toLong(), readyEvent.getUserId().toLong());
+    }
+
+    // 게임 시작 요청
+    @MessageMapping("/game.room.{roomId}.start")
+    fun startRoom(
+        @DestinationVariable roomId: Long,
+    ): ResponseEntity<RoomResponseDto> {
+        return ResponseEntity.ok().body(lobbyService.startRoom(roomId))
     }
 }
