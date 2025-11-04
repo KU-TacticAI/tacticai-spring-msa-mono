95aac0e064724d859ada3cc292748f5ba17ae5bc
K-da-fire <tmddnr3503@gmail.com>
2025-11-04 15:21:11 +0900

    헬스체크 추가

---
+++ core-service/src/main/java/com/example/coreservice/health/HealthCheckController.java
@@ -0,0 +1,19 @@
+package com.example.coreservice.health;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+@RequiredArgsConstructor
+public class HealthCheckController {
+
+  @GetMapping
+  public ResponseEntity<String> healthCheck(){
+    return ResponseEntity.ok().body("OK");
+  }
+
+}
