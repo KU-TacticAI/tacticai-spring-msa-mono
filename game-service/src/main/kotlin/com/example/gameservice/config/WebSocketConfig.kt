8b06c040478047d40110219d0e0191e333dd54e9
K-da-fire <tmddnr3503@gmail.com>
2025-11-12 19:35:14 +0900

    소켓 연결 문제 해결

--- game-service/src/main/kotlin/com/example/gameservice/config/WebSocketConfig.kt
+++ game-service/src/main/kotlin/com/example/gameservice/config/WebSocketConfig.kt
@@ -20,7 +20,7 @@ class WebSocketConfig : WebSocketMessageBrokerConfigurer {
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
         // 웹소켓 연결을 위한 엔드포인트 설정
         // 클라이언트는 /ws 주소로 STOMP 연결을 시도
-        registry.addEndpoint("/ws")
+        registry.addEndpoint("/api/game/ws")
 //            .setAllowedOrigins(
 //                "https://tacticai.site",
 //                "http://localhost:3000")
