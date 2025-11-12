93f78a6f42a3ebf9df65adb41a4bd075580540db
K-da-fire <tmddnr3503@gmail.com>
2025-11-12 16:12:51 +0900

    창을 닫았을 때 방에서 나가기 수정

--- game-service/src/main/kotlin/com/example/gameservice/config/WebSocketConfig.kt
+++ game-service/src/main/kotlin/com/example/gameservice/config/WebSocketConfig.kt
@@ -21,7 +21,7 @@ class WebSocketConfig : WebSocketMessageBrokerConfigurer {
         // 웹소켓 연결을 위한 엔드포인트 설정
         // 클라이언트는 /ws 주소로 STOMP 연결을 시도
         registry.addEndpoint("/ws")
-            .setAllowedOriginPatterns(
+            .setAllowedOrigins(
                 "https://tacticai.site",
                 "http://localhost:3000")
             .withSockJS() // SockJS를 사용하여 웹소켓을 지원하지 않는 브라우저에서도 통신 가능하도록 함
